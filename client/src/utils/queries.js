ec86278c047fff55babfae0fc939f1f102939ba3
Taylor Roberts <tstarrroberts@gmail.com>
2022-07-24 02:40:59 -0400

    add get me query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,15 @@
 import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    query me($username: Stirng, $email: String, $_id: ID) {
+        me(username: $username, email: $email,  _id: $_id) {
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+            }
+        }
+    }
+`;
