13edb473a7e64941922233c400567a43250aa986
Taylor Roberts <tstarrroberts@gmail.com>
2022-07-24 00:42:27 -0400

    fix server for graphql

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// remove routes add typedefs/reselvers and apollo + auth
+const { typeDefs, resolvers } = require('./schemas');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
+const server = new ApolloServer({ typeDefs, resolvers, context: authMiddleWare });
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,8 +18,18 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  // integrate apollo server with express as middleware
+  server.applyMiddleware({ app });
+};
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
+
+// call async function to start server
+startApolloServer(typeDefs, resolvers);
